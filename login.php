c5bea6143cd22792e5766de8387021b267e0e394
MathKasp <math.kasperczak@icloud.com>
2025-03-25 17:16:32 +0100

    feat() ajout fichiers

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+$pdo = new PDO("mysql:host=192.168.194.158;dbname=injection", "root", "secret", [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
+
+$username = htmlspecialchars($_POST['username']);
+$password = htmlspecialchars($_POST['password']);
+
+$stmt = $pdo->prepare("SELECT * FROM users WHERE username = :username AND password = :password");
+$stmt->execute([
+    'username' => $username,
+    'password' => $passwordt
+]);
+$resultat = $stmt->fetch();
+
+if ($resultat) {
+    echo "Connexion réussie !";
+} else {
+    echo "Mot de passe incorrect !";
+}
