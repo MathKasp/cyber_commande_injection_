285c360e765515980ca36dfc3f7e5de9da84523b
MathKasp <math.kasperczak@icloud.com>
2025-03-25 13:42:35 +0100

    initial commit

---
+++ command.php
@@ -0,0 +1,8 @@
+<?php
+if (isset($_GET['ip'])) {
+    $ip = $_GET['ip'];
+    $commande = shall_exec(command "ping -c 1" . $ip);
+    echo "<pre>" . $commande . "</pre>";
+} else {
+    echo "Veuillez fournir une adresse IP";
+}
