c237921a25e2629e4e6f79cf63d1f9610e9bfae1
MathKasp <math.kasperczak@icloud.com>
2025-03-25 15:28:24 +0100

    feat(command.php) sécurisation

--- command.php
+++ command.php
@@ -1,6 +1,6 @@
 <?php 
 if (isset($_GET['ip'])){
-    $ip = $_GET['ip'];
+    $ip = escapeshellarg($_GET['ip']);
     $commande = shell_exec(command:"ping -c 1 " .$ip);
     echo "<pre>". $commande . "</pre>";
 } else {
