adc967b7e63926797e5b1ee5be6dc85fa12007b3
MathKasp <math.kasperczak@icloud.com>
2025-03-25 15:03:33 +0100

    fix(commande.php) espace de tes morts

--- command.php
+++ command.php
@@ -1,7 +1,7 @@
 <?php 
 if (isset($_GET['ip'])){
     $ip = $_GET['ip'];
-    $commande = shell_exec(command:"ping -c 1" .$ip);
+    $commande = shell_exec(command:"ping -c 1 " .$ip);
     echo "<pre>". $commande . "</pre>";
 } else {
     echo "Veuillez fournir une IP";
